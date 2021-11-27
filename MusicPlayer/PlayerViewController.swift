4da71824f67f2007da69d489e8ac07ec15e0808e
Valerii <teslareiden@gmail.com>
2021-11-27 18:33:41 +0200

    Add AVAudioPlayer

--- MusicPlayer/PlayerViewController.swift
+++ MusicPlayer/PlayerViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     public var position = 0
     public var songs: [Song] = []
     
@@ -23,14 +26,37 @@ class PlayerViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        if holder.subviews.count == 1 {
+        if holder.subviews.count == 0 {
             configure()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let player = player {
+            player.stop()
         }
     }
    
     func configure() {
         // set up player
+        let song = songs[position]
+        guard let urlString = Bundle.main.url(forResource: song.trackName, withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            player = try AVAudioPlayer(contentsOf: urlString)
+            
+            player?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
         
         //set up user interface elements
     }
+    
+    
 }
