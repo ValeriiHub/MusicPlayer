2d9874af9c8723dadab3f329226b79d4f075f01f
Valerii <teslareiden@gmail.com>
2021-11-27 14:27:13 +0200

    Add tableView and Song model

---
+++ MusicPlayer/Model/Song.swift
@@ -0,0 +1,16 @@
+//
+//  Song.swift
+//  MusicPlayer
+//
+//  Created by Valerii D on 26.11.2021.
+//
+
+import Foundation
+
+struct Song {
+    let name: String
+    let albumName: String
+    let artistName: String
+    let imageName: String
+    let trackName: String
+}
--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -8,12 +8,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var table: UITableView!
+    
+    var songs: [Song] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        getSongs()
     }
 
-
+    func getSongs() {
+        songs.append(Song(name: "Background music",
+                          albumName: "Other",
+                          artistName: "Rnado",
+                          imageName: "image1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla",
+                          imageName: "image2",
+                          trackName: "song2"))
+        songs.append(Song(name: "Viva la vida",
+                          albumName: "Something",
+                          artistName: "Coldplay",
+                          imageName: "image3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Background music",
+                          albumName: "Other",
+                          artistName: "Rnado",
+                          imageName: "image1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla",
+                          imageName: "image2",
+                          trackName: "song2"))
+        songs.append(Song(name: "Viva la vida",
+                          albumName: "Something",
+                          artistName: "Coldplay",
+                          imageName: "image3",
+                          trackName: "song3"))
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let song = songs[indexPath.row]
+        
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.albumName
+        cell.imageView?.image = UIImage(named: song.imageName)
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica-Bold", size: 16)
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // present the player
+        let position = indexPath.row
+        
+        //song
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "player") else { return}
+        present(vc, animated: true)
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+}
